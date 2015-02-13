f5034871b749ee5a85a1a0003ac7d3567d3c5533
bmaupin <bmaupin@users.noreply.github.com>
2015-02-13 14:14:35 -0500

    Install desired packages and install Deja Dup at work too

--- ubuntu-common/setup.sh
+++ ubuntu-common/setup.sh
@@ -14,7 +14,7 @@ ln -s $SCRIPTPATH/.gitconfig ~/.gitconfig
 ln -s $SCRIPTPATH/.vimrc ~/.vimrc
 
 # Install packages
-sudo apt-get install -y aptitude icedtea-7-plugin openjdk-7-jdk
+sudo apt-get install -y aptitude brasero deja-dup gedit gnote icedtea-7-plugin libreoffice openjdk-7-jdk rhythmbox shotwell
 
 # Set up firewall
 sudo ufw default deny  # (defaults to blocking all incoming connections)
@@ -32,17 +32,14 @@ sudo sed -i.bak 's/^#GRUB_DISABLE_RECOVERY="true"$/GRUB_DISABLE_RECOVERY="true"/
 sudo chmod -x /etc/grub.d/20_memtest86+
 sudo update-grub
 
+# Configure Deja Dup (Backup) full backup period
+gsettings set org.gnome.DejaDup full-backup-period 180
+gsettings get org.gnome.DejaDup full-backup-period
+
 
 # Location-based configuration
 if [ "$LOCATION" = "home" ]
 then
-    # Install packages
-    sudo apt-get -y install deja-dup
-
     # Disable daily mlocate update
     sudo chmod -x /etc/cron.daily/mlocate
-
-    # Configure Deja Dup (Backup) full backup period
-    gsettings set org.gnome.DejaDup full-backup-period 180
-    gsettings get org.gnome.DejaDup full-backup-period
 fi
