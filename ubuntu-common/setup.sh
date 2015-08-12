3e68325fd1b3306996c39c0c66553375c7a0627a
bmaupin <bmaupin@users.noreply.github.com>
2015-08-12 10:07:59 -0400

    libfreerdp-plugins-standard needs to be updated as well

--- ubuntu-common/setup.sh
+++ ubuntu-common/setup.sh
@@ -29,7 +29,7 @@ if lsb_release -r | grep -q 14.04; then
 
     sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
     sudo apt-get update
-    sudo apt-get install -y remmina
+    sudo apt-get install -y libfreerdp-plugins-standard remmina
 fi
 
 # Update apt-file cache
