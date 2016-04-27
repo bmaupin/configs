21158f47ab8a2e29893d0cf1c0cfd921cd5641b0
bmaupin <bmaupin@users.noreply.github.com>
2016-04-27 17:51:18 -0400

    Use wincinderith/topicons after all

--- ubuntu-gnome/setup.sh
+++ ubuntu-gnome/setup.sh
@@ -34,33 +34,15 @@ gsettings set org.gnome.nautilus.preferences enable-delete true
 
 # Move tray icons to the top
 mkdir -p ~/.local/share/gnome-shell/extensions
-#mkdir -p ~/workspace/git/
-#cd ~/workspace/git
-#git clone http://94.247.144.115/~git/topicons.git
-#cp -rT topicons/ ~/.local/share/gnome-shell/extensions/topIcons@adel.gadllah@gmail.com
-# The original repo is timing out
-wget https://repo.dray.be/package-files/topicons-28.tar.gz
-tar -xf topicons-28.tar.gz
-mv -T topicons-28 ~/.local/share/gnome-shell/extensions/topIcons@adel.gadllah@gmail.com
-rm -rf topicons-28*
-# A couple tweaks to make padding more reasonable
-sed -i.bak 's/const PANEL_ICON_SIZE = 24;/const PANEL_ICON_SIZE = 18;\nconst PANEL_ICON_PADDING = 0;/' ~/.local/share/gnome-shell/extensions/topIcons@adel.gadllah@gmail.com/extension.js
-sed -i '/box.add_actor(icon);/a \    box.set_style("-natural-hpadding: %dpx".format(PANEL_ICON_PADDING));' ~/.local/share/gnome-shell/extensions/topIcons@adel.gadllah@gmail.com/extension.js
-gsettings set org.gnome.shell enabled-extensions "$(gsettings get org.gnome.shell enabled-extensions | sed -e "s/']/', ]/" | sed -e "s/]$/'topIcons@adel.gadllah@gmail.com']/")"
-killall -3 gnome-shell
-
-# This one seems to only show one icon at a time unless the size is changed
-#mkdir -p ~/workspace/git/
-#cd ~/workspace/git/
-#git clone https://github.com/wincinderith/topicons
-#cd topicons
-#cp -rT topicons/ ~/.local/share/gnome-shell/extensions/topIcons@kevinboxhoorn.yahoo.com
-#sudo cp ~/workspace/git/topicons/schemas/org.gnome.shell.extensions.topicons.gschema.xml /usr/share/glib-2.0/schemas/
-#cd /usr/share/glib-2.0/schemas
-#sudo glib-compile-schemas .
-#gsettings set org.gnome.shell.extensions.topicons icon-padding 0
-#gsettings set org.gnome.shell enabled-extensions "$(gsettings get org.gnome.shell enabled-extensions | sed -e "s/']/', ]/" | sed -e "s/]$/'topIcons@kevinboxhoorn.yahoo.com']/")"
-
+mkdir -p ~/workspace/git/
+cd ~/workspace/git/
+git clone https://github.com/wincinderith/topicons
+cp -rT topicons/ ~/.local/share/gnome-shell/extensions/topIcons@kevinboxhoorn.yahoo.com
+sudo cp ~/workspace/git/topicons/schemas/org.gnome.shell.extensions.topicons.gschema.xml /usr/share/glib-2.0/schemas/
+cd /usr/share/glib-2.0/schemas
+sudo glib-compile-schemas .
+gsettings set org.gnome.shell.extensions.topicons icon-padding 0
+gsettings set org.gnome.shell enabled-extensions "$(gsettings get org.gnome.shell enabled-extensions | sed -e "s/']/', ]/" | sed -e "s/]$/'topIcons@kevinboxhoorn.yahoo.com']/")"
 
 # Install system monitor
 mkdir -p ~/.local/share/gnome-shell/extensions
